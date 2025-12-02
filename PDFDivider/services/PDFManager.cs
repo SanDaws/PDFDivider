42a23d5bc5339d3d9b5b18026c4ff1a68a449e19
Daws <castrogiraldosantiago@gmail.com>
2025-12-02 17:16:54 -0500

    centralizacion de tareas con pdfs

---
+++ PDFDivider/services/PDFManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class PdfManager
+{
+
+    //ruta de entrada del pdf
+    public string FilePath { get; set; }
+    public string OutputDirectory { get; set; }
+    public int initialPage { get; set; }
+    public int finalPage { get; set; }
+    public
+
+    //constructor
+    public PdfManager(string filePath)
+    {
+        FilePath = filePath;
+    }
+    //verificadores
+    //verificar si el archivo es un pdf
+    public bool IsValidPdf()
+    {
+        // Verificar si el archivo tiene la extensión .pdf
+        return FilePath.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase);
+    }
+    //vrificar si el archivo existe
+    //crear runta de salida
+
+    //setter
+    //pagina inicial y final
+    //output directory
+
+
+
+}
