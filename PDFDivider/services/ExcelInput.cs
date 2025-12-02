12c4b6eddcb3e5b97e2893e28a67c1a4d263544e
Daws <castrogiraldosantiago@gmail.com>
2025-12-02 18:00:45 -0500

    instalacion de libreria y verificadores baSICOS DE CLASES

--- PDFDivider/services/ExcelInput.cs
+++ PDFDivider/services/ExcelInput.cs
@@ -2,7 +2,28 @@
 
 public class ExcelInput
 {
-	public ExcelInput()
+	public excelpath { get; set; }
+	public ExcelInput( string path)
 	{
-	}
+		excelpath = path;
+}
+//verificar archivo excel
+	public bool VerifyExcelFile()
+	{
+		// Lógica para verificar si el archivo de Excel existe y es accesible
+		return System.IO.File.Exists(excelpath);
+}
+//abrir archivo excel
+	public void OpenExcelFile()
+	{
+		if (VerifyExcelFile())
+		{
+			// Lógica para abrir y leer el archivo de Excel
+			Console.WriteLine("Archivo de Excel abierto correctamente.");
+		}
+		else
+		{
+			Console.WriteLine("El archivo de Excel no existe o no es accesible.");
+		}
+}
 }
--- PDFDivider/services/PDFManager.cs
+++ PDFDivider/services/PDFManager.cs
@@ -8,7 +8,7 @@ public class PdfManager
     public string OutputDirectory { get; set; }
     public int initialPage { get; set; }
     public int finalPage { get; set; }
-    public
+    public list<Ximages>
 
     //constructor
     public PdfManager(string filePath)
@@ -16,6 +16,7 @@ public class PdfManager
         FilePath = filePath;
     }
     //verificadores
+    //verificar si hay archivo pdf
     //verificar si el archivo es un pdf
     public bool IsValidPdf()
     {
@@ -23,11 +24,51 @@ public class PdfManager
         return FilePath.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase);
     }
     //vrificar si el archivo existe
+    public bool FileExists(string filePath)
+    {
+        return System.IO.File.Exists(filePath);
+    }
+    //verificar si la ruta de salida existe
+    public bool OutputDirectoryExists(string outputDirectory)
+    {
+        //true si existe, false si no
+        return System.IO.Directory.Exists(outputDirectory);
+    }
     //crear runta de salida
+    public string directoryCreate(string GeneralDirectory, string newDirectoryName)
+    {
+
+        if (!OutputDirectory(GeneralDirectory))
+        {
+         return null;
+        }
+        string newDirectoryPath = $"{GeneralDirectory}\\{newDirectoryName}";
+        System.IO.Directory.CreateDirectory(newDirectoryName);
+        return newDirectoryPath;
+
+
+    }
 
     //setter
+    public void SetOutputDirectory(string outputDirectory)
+    {
+        OutputDirectory = outputDirectory;
+    }
     //pagina inicial y final
+    public void rangePages(int initial, int final)
+    {
+        initialPage = initial;
+        finalPage = final;
+    }
     //output directory
+    public bool safeoutputdirectory(string generalDirectory)
+    {
+        if (!OutputDirectoryExists(generalDirectory))
+        {
+            directoryCreate(generalDirectory, "PDFDividerOutput");
+        }
+        return true;
+    }
 
 
 
